953b11742971c4691be2b3318483ba4ae50ae48f
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-11-01 12:59:22 -0700

    puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,34 @@
+#Install Nginx web server (w/ Puppet)
+
+class { 'nginx':
+  package_manage => 'present',
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'file',
+  content => "Hello World!\n",
+  require => Class['nginx'],
+}
+
+nginx::resource::server { 'default':
+  server_name => '_',
+  location    => {
+    '/'          => {
+      root => '/var/www/html',
+      index => 'index.nginx-debian.html',
+    },
+    '/redirect_me' => {
+      rewrite => 'https://github.com/Mufidat-Ahmed permanent',
+    },
+    '/404.html'   => {
+      internal => 'true',
+    },
+  },
+  require     => File['/var/www/html/index.nginx-debian.html'],
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Class['nginx'],
+}
