3dac3e7780bbaeb293283e1bdfe6e2ca69d93c93
Mufidat-Ahmed <imufidatahmad@gmail.com>
2023-11-27 08:29:05 -0800

    Debug

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,11 +8,11 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(user_id)).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w") as jsonfile:
+    with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
