6e84cb9a6b5b7397ad639e57d4099666f68328c3
Mufidat-Ahmed <imufidatahmad@gmail.com>
2023-12-12 04:43:58 -0800

    count it

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""returns the number of subscribers for a given subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return total subscribers for a subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    url = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\ v1.0.0'}).json()
+    subs = url.get("data", {}).get("subscribers", 0)
+    return subs
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""prints the titles of the first 10 hot posts listed for a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """Print the post titles if subreddit is valid"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    ten = response.json().get("data")
+    [print(i.get("data").get("title")) for i in ten.get("children")]
