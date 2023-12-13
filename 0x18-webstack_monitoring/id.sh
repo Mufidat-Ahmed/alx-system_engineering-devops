636e46a6c742579c37c5cb3810722ef6fc6acccf
Mufidat-Ahmed <imufidatahmad@gmail.com>
2023-12-13 08:37:24 -0800

    edit

--- 0x18-webstack_monitoring/id.sh
+++
@@ -1,9 +0,0 @@
-# Replace YOUR_API_KEY and YOUR_APP_KEY with your actual Datadog API key and Application key
-export DD_API_KEY=f71b19c8dc3d57800cc4937b58f4d836
-export DD_APP_KEY=633f35c08697dfce945ca4c167eb070acf3928dc
-
-# Replace YOUR_ORG_NAME with your actual Datadog organization name
-export DD_ORG_NAME="Alx"
-
-# List all dashboards
-curl -X GET "https://api.datadoghq.com/api/v1/dashboard/lists/manual/${DD_ORG_NAME}?api_key=${DD_API_KEY}&application_key=${DD_APP_KEY}"
