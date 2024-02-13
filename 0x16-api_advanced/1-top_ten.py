086bc38f0c78f8b26bde6679c30fffd7ef449ce7
Mufidat-Ahmed <imufidatahmad@gmail.com>
2024-02-13 16:40:43 +0000

    Top-ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,18 +5,15 @@ import requests
 
 def top_ten(subreddit):
     """Print the post titles if subreddit is valid"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+    url = "https://www.reddit.com/r/{}/top/.json?limit=10".format(subreddit)
     headers = {
                   "User-Agent": "0x16-api_advanced:project:\
                    v1.0.0"
                     }
-    params = {
-              "limit": 10
-        }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
-    ten = response.json().get("data")
-    [print(i.get("data").get("title")) for i in ten.get("children")]
+    ten = response.json().get("data").get("children")
+    [print(i.get("data").get("title")) for i in ten]
