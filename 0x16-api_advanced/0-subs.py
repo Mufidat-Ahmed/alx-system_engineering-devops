65eeceea93575640addfbc8ad9213214a14df46f
Mufidat-Ahmed <imufidatahmad@gmail.com>
2024-02-13 15:35:52 +0100

    Edit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns the number of subscribers """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+  """ Return total subscribers for a subreddit"""
+  if subreddit is None or type(subreddit) is not str:
+    return 0
+  u = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+            v1.0.0'}).json()
+  subsred = u.get("data", {}).get("subscribers", 0)
+  return subsred
