4f317a0859436157a6d45a63c08681e143df8a56
Mufidat-Ahmed <imufidatahmad@gmail.com>
2024-02-14 21:35:39 +0000

    1-top_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,19 @@
-#!usr/bin/python3
-"""Queries the reddit api and return the top 10 hot post"""
-
+#!/usr/bin/python3
+"""prints the titles of the first 10 hot posts listed for a given subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts in the given subreddit."""
+    """Print the post titles if subreddit is valid"""
 
-    url = "https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{}/top/.json?limit=10".format(subreddit)
     headers = {
-        "User-Agent": "TopTenHotPosts/1.0.0 (by /u/0x16-api_advanced:project)"
-    }
-
+                  "User-Agent": "0x16-api_advanced:project:\
+                   v1.0.0"
+                    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 404:
         print("None")
         return
-    else:
-        data = response.json()
-        for post in data['data']['children']:
-            print(post['data']['title'])
+    ten = response.json().get("data").get("children")
+    [print(i.get("data").get("title")) for i in ten]
